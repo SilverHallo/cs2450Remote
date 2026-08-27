8944c5413383e37c2e856ff3a8b63e2e0541e3f8
Diogo <diogoespino2@gmail.com>
2026-08-27 13:10:29 -0600

    First Commit

---
+++ age_guesser.py
@@ -0,0 +1,20 @@
+import random
+
+def main():
+	print("Hello. I am going to guess your age.")
+	guess_name()
+
+def guess_name():
+	
+	name = input("What is your name? ")
+	guess_right = False
+	while not guess_right:
+		
+		age = random.randint(15, 40)
+		guess = input("Is your age " + str(age) + "? y/n" )
+		if guess == y:
+			print(name + " is " + age + " years old."
+			guess_right = True
+		elif guess == n:
+			print("Rats.")
+	return
